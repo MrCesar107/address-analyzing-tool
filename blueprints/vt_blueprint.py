302c639959e577432f6e40552cda8d23bc62a35d
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2024-12-03 15:22:35 -0600

    Add dotenv & virustotal api

--- blueprints/vt_blueprint.py
+++ blueprints/vt_blueprint.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request
 import vt
+import os
 
 vt_blueprint = Blueprint("vt", __name__, url_prefix="/vt")
 
-client = vt.Client("")
+client = vt.Client(os.getenv('VT_API_KEY'))
 
 @vt_blueprint.route("/analize_address", methods=["POST"])
 def analize_address():
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, url_for
+from dotenv import load_dotenv
 from assets_blueprint import assets_blueprint
 from blueprints.vt_blueprint import vt_blueprint
+import os
+
+load_dotenv()
 
 app = Flask(
   __name__,
@@ -8,6 +12,8 @@ app = Flask(
   static_folder="public",
   template_folder="templates"
 )
+
+app.config['VT_API_KEY'] = os.getenv("VT_API_KEY")
 
 # Provide Vite context processors and static assets directory
 app.register_blueprint(assets_blueprint)
