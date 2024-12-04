f4014e169b3e61015c6b4adf2311f349671cdc00
Cesar Rodriguez <cesar.rodriguez.lara54@gmail.com>
2024-12-04 15:47:05 -0600

    Remove VT client and use API directly instead

--- blueprints/vt_blueprint.py
+++ blueprints/vt_blueprint.py
@@ -1,12 +1,52 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+import requests
 import vt
 import os
 
 vt_blueprint = Blueprint("vt", __name__, url_prefix="/vt")
 
-client = vt.Client(os.getenv('VT_API_KEY'))
-
 @vt_blueprint.route("/analize_address", methods=["POST"])
 def analize_address():
-  address = request.form['address']
-  return f"Formulario recibido Address: {address}"
+  address = request.form.get("address")
+  analysis = analize_url(address)
+  return analysis
+
+@vt_blueprint.route("/get_url_analysis/<analysis_id>", methods=["GET"])
+def get_url_analysis(analysis_id):
+  url = f"{build_endpoint("analyses")}/{analysis_id}"
+  headers = {
+    "accept": "application/json",
+    "x-apikey": os.getenv("VT_API_KEY")
+  }
+  response = requests.get(url, headers=headers)
+
+  if response.status_code == 200:
+    result = response.json()
+    return jsonify(result)
+  else:
+    return f"Hubo un error al obtener los resultados: {response.text}", response.status_code
+
+
+def analize_url(url):
+  if not url:
+    return jsonify({"error": "No se proporcionó una URL"}), 400
+
+  endpoint = build_endpoint("urls")
+  headers = {
+    "accept": "application/json",
+    "x-apikey": os.getenv("VT_API_KEY"),
+    "content-type": "application/x-www-form-urlencoded"
+  }
+  data = { "url": url }
+  response = requests.post(endpoint, headers=headers, data=data)
+
+  if response.status_code == 200:
+    result = response.json()
+    analysis_id = result["data"]["id"]
+    return jsonify({"message": "URL enviada para análisis", "analysis_id": analysis_id})
+  else:
+    return jsonify({"error": "Error al enviar la URL", "details": response.text}), response.status_code
+
+
+def build_endpoint(path):
+  return f"{os.getenv("VT_URL_ENDPOINT")}/{path}"
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, jsonify
 from dotenv import load_dotenv
 from assets_blueprint import assets_blueprint
 from blueprints.vt_blueprint import vt_blueprint
@@ -14,6 +14,7 @@ app = Flask(
 )
 
 app.config['VT_API_KEY'] = os.getenv("VT_API_KEY")
+app.config['VT_URL_ENDPOINT'] = os.getenv("VT_URL_ENDPOINT")
 
 # Provide Vite context processors and static assets directory
 app.register_blueprint(assets_blueprint)
@@ -23,3 +24,7 @@ app.register_blueprint(vt_blueprint)
 @app.get("/")
 def index():
   return render_template("index.html")
+
+@app.get("/test")
+def test():
+  return jsonify({"key": os.getenv("VT_API_KEY")})
